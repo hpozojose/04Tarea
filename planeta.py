b7d923fea648aaea9fb4143f5d0acda022f0f5a4
hpozojose <hpozo.jose@gmail.com>
2015-10-21 11:19:23 -0300

    Construccion clase planeta y intento de aplicacion en solucion con euler, no funciona hasta el momento

--- planeta.py
+++ planeta.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import numpy as np
 class Planeta(object):
+
+G=1
+M=1
     '''
-    Complete el docstring.
+    La clase planeta contiene funciones para integrar el potencial usando Euler,
+    Runge-Kutta y Verlet, tambien se calcular con esta clase la energia de un
+     planeta cualquiera, dependera del delta tiempo, la masa del
+    planeta que sufre la atraccion, la constante alpha y las condiciones iniciales
+    dadas
     '''
 
-    def __init__(self, condicion_inicial, alpha=0):
+    def __init__(self, condicion_inicial, alpha=0, masa=1):
         '''
-        __init__ es un método especial que se usa para inicializar las
+        __init__ es un metodo especial que se usa para inicializar las
         instancias de una clase.
-
-        Ej. de uso:
-        >> mercurio = Planeta([x0, y0, vx0, vy0])
-        >> print(mercurio.alpha)
-        >> 0.
         '''
+
+        self.y_anterior = np.array([])
         self.y_actual = condicion_inicial
         self.t_actual = 0.
         self.alpha = alpha
+        self.m = masa
 
     def ecuacion_de_movimiento(self):
         '''
@@ -26,8 +31,13 @@ class Planeta(object):
         primer orden.
         '''
         x, y, vx, vy = self.y_actual
-        # fx = ...
-        # fy = ...
+        radio = np.sqrt(x**2 + y**2)
+        cos_r = x/radio
+        sen_r = y/radio
+        a = G * M * ((2*self.alpha/(radio**3)) - 1/(radio**2))
+        fx = cos_r * a
+        fy = sen_r * a
+
         return [vx, vy, fx, fy]
 
     def avanza_euler(self, dt):
@@ -36,24 +46,54 @@ class Planeta(object):
         en un intervalo de tiempo dt usando el método de Euler explícito. El
         método no retorna nada, pero re-setea los valores de self.y_actual.
         '''
+        y_init = self.y_actual
+        der_y_init = self.ecuacion_de_movimiento()
+        self.y_anterior = y_init
+        self.y_actual = y_init + dt * der_y_init
+        self.t_actual += dt
         pass
 
     def avanza_rk4(self, dt):
         '''
         Similar a avanza_euler, pero usando Runge-Kutta 4.
         '''
+        y_init = self.y_actual
+        K1 = self.ecuacion_de_movimiento()
+        self.y_actual = y_init + dt*K1/2.
+        K2 = self.ecuacion_de_movimiento()
+        self.y_actual = y_init + dt*K2/2.
+        K3 = self.ecuacion_de_movimiento()
+        self.y_actual = y_init + dt*K3
+        K4 = self.ecuacion_de_movimiento()
+
+        self.y_anterior = y_init
+        self.y_actual = y_init + (K1+2*K2+2*K3+K4)*dt/6.
+        self.t_actual += dt
         pass
 
     def avanza_verlet(self, dt):
         '''
         Similar a avanza_euler, pero usando Verlet.
         '''
+        y_init = self.y_actual
+        der_y_n = self.ecuacion_de_movimiento()
+        y_n1 = 2*self.y_actual[0:2] - self.y_anterior[0:2] + (dt**2)*der_y_n[2:]
+        dy_n1 = (y_n1 - self.y_actual[0:2])/dt
+
+        self.y_anterior = y_init
+        self.y_actual = np.concatenate((y_n1,dy_n1))
+        self.t_actual += dt
+
         pass
 
     def energia_total(self):
         '''
         Calcula la enérgía total del sistema en las condiciones actuales.
         '''
-        pass
+        x, y, vx, vy = self.y_actual
+        radio = np.sqrt(x**2 + y**2)
+        U = - G * M * self.m/radio  + (self.alpha * G * M * self.m /(radio**2))
+        K = (vx**2 + vt**2) * self.m/2.
+        Energia = K + U
 
-
+        return Energia
--- solucion_usando_euler.py
+++ solucion_usando_euler.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from planeta import Planeta
-
-
-condicion_inicial = [10, 0, 0, XXX]
-
-p = Planeta(condicion_inicial)
-
 '''
 Complete el código a continuación.
 '''
+from planeta import Planeta
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Solucion a trayectoria
+condicion_inicial = [10, 0, 0, 0.3]
+
+Jupiter = Planeta(condicion_inicial)
+x_value = []
+y_value = []
+E = []
+dt=0.1
+pasos=1000
+t_values = np.array(range(pasos))*dt
+
+for i in range(pasos):
+    x_value.append(Jupiter.y_actual[0])
+    y_value.append(Jupiter.y_actual[1])
+    E.append(Jupiter.energia_total())
+    Jupiter.avanza_euler(dt)
+
+x_value = np.array(x_pos)
+y_value = np.array(y_pos)
+energia = np.array(energia)
+
+fig=plt.figure(1)
+plt.subplot(2, 1, 1)
+fig.subplots_adjust(hspace=.5)
+plt.plot(x_value , y_value)
+plt.title(' 'u'Ó''rbita descrita por planeta (m'u'é''todo de Euler)')
+plt.xlabel("x [m]")
+plt.ylabel("y [m]")
+plt.savefig('Orbita_Euler.eps')
+plt.show()
+
+
+
+plt.subplot(2, 1, 2)
+plt.plot(t_values, Energia)
+plt.title("Energia a traves del tiempo")
+plt.xlabel("Tiempo [s]")
+plt.ylabel("Energia [J]")
+
+plt.savefig("Energia_Euler.eps")
+plt.show()
